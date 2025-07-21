032a8fac4aa1c5471a34c20c50ffc8be8e7f50ed
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-20 23:31:41 -0500

    Add a finctions advance about of project Holberton in binary tree

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,80 +1,88 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * rebalance_node - Rebalances a subtree rooted at node
- * @node: Pointer to the root of the subtree
- *
- * Return: New root after rebalancing
+ * min_value_node - Encuentra el nodo con el valor mínimo en un subárbol.
+ * @node: Puntero a la raíz del subárbol.
+ * Return: Puntero al nodo con el menor valor.
  */
-static avl_t *rebalance_node(avl_t *node)
+avl_t *min_value_node(avl_t *node)
 {
-	int balance = binary_tree_balance(node);
-
-	if (balance > 1)
-	{
-		if (binary_tree_balance(node->left) < 0)
-			node->left = binary_tree_rotate_left(node->left);
-		return (binary_tree_rotate_right(node));
-	}
-	if (balance < -1)
-	{
-		if (binary_tree_balance(node->right) > 0)
-			node->right = binary_tree_rotate_right(node->right);
-		return (binary_tree_rotate_left(node));
-	}
+	while (node->left)
+		node = node->left;
 	return (node);
 }
 
 /**
- * avl_remove_rec - Recursively removes value and rebalances
- * @root: Pointer to current subtree root
- * @value: Value to remove
- *
- * Return: New root after removal and rebalance
+ * avl_delete_node - Elimina un nodo dado de un árbol AVL.
+ * @root: Puntero al nodo raíz del subárbol.
+ * Return: Puntero al nuevo nodo raíz del subárbol.
  */
-static avl_t *avl_remove_rec(avl_t *root, int value)
+avl_t *avl_delete_node(avl_t *root)
 {
-	avl_t *succ;
+	avl_t *temp;
+
+	if (!root->left || !root->right)
+	{
+		temp = root->left ? root->left : root->right;
+		if (!temp)
+		{
+			temp = root;
+			root = NULL;
+		}
+		else
+		{
+			temp->parent = root->parent;
+			*root = *temp;
+		}
+		free(temp);
+	}
+	else
+	{
+		temp = min_value_node(root->right);
+		root->n = temp->n;
+		root->right = avl_remove(root->right, temp->n);
+	}
+	return (root);
+}
+
+/**
+ * avl_remove - Elimina un nodo de un árbol AVL y re-balancea.
+ * @root: Puntero a la raíz del árbol.
+ * @value: Valor a eliminar.
+ * Return: Puntero a la nueva raíz tras re-balancear.
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+	int balance;
 
 	if (!root)
 		return (NULL);
 
 	if (value < root->n)
-		root->left = avl_remove_rec(root->left, value);
+		root->left = avl_remove(root->left, value);
 	else if (value > root->n)
-		root->right = avl_remove_rec(root->right, value);
+		root->right = avl_remove(root->right, value);
 	else
+		root = avl_delete_node(root);
+
+	if (!root)
+		return (NULL);
+
+	balance = binary_tree_balance(root);
+
+	if (balance > 1 && binary_tree_balance(root->left) >= 0)
+		return (binary_tree_rotate_right(root));
+	if (balance > 1 && binary_tree_balance(root->left) < 0)
 	{
-		if (!root->left || !root->right)
-		{
-			succ = root->left ? root->left : root->right;
-			free(root);
-			return (succ);
-		}
-		succ = root->right;
-		while (succ->left)
-			succ = succ->left;
-		root->n = succ->n;
-		root->right = avl_remove_rec(root->right, succ->n);
+		root->left = binary_tree_rotate_left(root->left);
+		return (binary_tree_rotate_right(root));
 	}
-	if (root)
-		root = rebalance_node(root);
+	if (balance < -1 && binary_tree_balance(root->right) <= 0)
+		return (binary_tree_rotate_left(root));
+	if (balance < -1 && binary_tree_balance(root->right) > 0)
+	{
+		root->right = binary_tree_rotate_right(root->right);
+		return (binary_tree_rotate_left(root));
+	}
 	return (root);
-}
-
-/**
- * avl_remove - Removes a node from an AVL tree and rebalances
- * @root: Pointer to the root node of the AVL tree
- * @value: Value to remove
- *
- * Return: Pointer to the new root after removal
- */
-avl_t *avl_remove(avl_t *root, int value)
-{
-	avl_t *new_root = avl_remove_rec(root, value);
-
-	if (new_root)
-		new_root->parent = NULL;
-	return (new_root);
 }
