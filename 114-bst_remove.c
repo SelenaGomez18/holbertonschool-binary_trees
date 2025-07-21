c259c35daa806aaac2966d0e9ce98556762d6e46
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-20 22:18:52 -0500

    Add a finctions advance about of project Holberton in binary tree

---
+++ 114-bst_remove.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * bst_remove - Remove a node from a BST
+ * @root: Pointer to root node
+ * @value: Value to remove
+ *
+ * Return: New root after deletion
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp;
+
+	if (!root)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (!root->left)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if (!root->right)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+		temp = root->right;
+		while (temp->left)
+			temp = temp->left;
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+	}
+	return (root);
+}
